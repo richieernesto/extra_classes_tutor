9b84b0c019da4f82b8cd66ca6f67d5a8caf337bf
Richie <esselrichmond78@gmail.com>
2022-05-30 07:32:30 +0000

    Auth Screen modified

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -361,41 +361,6 @@ class _AuthScreenState extends State<AuthScreen> {
       ),
     );
   }
-
-  Widget textBox(
-      IconData icon, String hintText, bool isPassword, bool isEmail) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      height: size.width / 8,
-      width: size.width / 1.25,
-      alignment: Alignment.center,
-      padding: EdgeInsets.only(right: size.width / 30),
-      decoration: BoxDecoration(
-        color: Colors.black.withOpacity(.1),
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: TextFormField(
-        style: TextStyle(
-          color: Colors.white.withOpacity(.9),
-        ),
-        obscureText: isPassword,
-        keyboardType: isEmail ? TextInputType.emailAddress : TextInputType.text,
-        decoration: InputDecoration(
-          prefixIcon: Icon(
-            icon,
-            color: Colors.white.withOpacity(.8),
-          ),
-          border: InputBorder.none,
-          hintMaxLines: 1,
-          hintText: hintText,
-          hintStyle: TextStyle(
-            fontSize: 14,
-            color: Colors.white.withOpacity(.5),
-          ),
-        ),
-      ),
-    );
-  }
 }
 
 class MyBehavior extends ScrollBehavior {
